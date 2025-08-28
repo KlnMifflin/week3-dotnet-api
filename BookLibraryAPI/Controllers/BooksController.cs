49dc1de61f9ef23accaecee8292dc274253fe33c
KlnMifflin <74214270+KlnMifflin@users.noreply.github.com>
2025-08-28 12:47:21 -0400

    Add Files For BookLibraryAPI via Upload
    Add files to allow backend creation, reading, updating, and deletion of books in the library. Test through Swagger.

---
+++ BookLibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookLibraryAPI.Models;
+
+namespace BookLibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public static List<Book> books = new List<Book> { }; //hold books in library
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetAllBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id); //find book with matching id
+
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> AddBook(Book book)
+        {
+            book.Id = books.Count == 0 ? 1 : books.Max(x => x.Id) + 1; //autoincrement id
+            books.Add(book);
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Book> UpdateBook(int id, Book newBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null) return NotFound();
+
+            book.Title = newBook.Title;
+            book.Author = newBook.Author;
+            book.Year = newBook.Year;
+            book.Description = newBook.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Book> DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null) return NotFound();
+
+            books.Remove(book); //remove book from list
+            return NoContent();
+        }
+    }
+}
---
+++ BookLibraryAPI/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookLibraryAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Author { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
