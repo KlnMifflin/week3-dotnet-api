7b1757ebfd4d6232dc2cd4755f7b0780206f0fae
KlnMifflin <74214270+KlnMifflin@users.noreply.github.com>
2025-08-28 21:29:11 -0400

    Update Book.cs
    Minor edits to allow null description.

--- BookLibraryAPI/Models/Book.cs
+++ BookLibraryAPI/Models/Book.cs
@@ -8,13 +8,14 @@ namespace BookLibraryAPI.Models
         public int Id { get; set; }
 
         [Required]
-        public string Title { get; set; } = string.Empty;
+        public string Title { get; set; }
 
         [Required]
-        public string Author { get; set; } = string.Empty;
+        public string Author { get; set; }
 
         public int Year { get; set; }
 
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; }
     }
+
 }
